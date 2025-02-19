e2ea30e05b907016705b11b7b763a6aea1a2b6ca
Emerson Vieira <emerson.chaves@nuvem.net>
2025-02-19 19:58:28 -0400

    feat: create input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Copy } from 'lucide-react';
-import { Button } from "@/components/button";
-import { IconButton } from '@/components/icon-button';
+import { Button } from '@/components/button'
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+
+          <InputField placeholder="Digite seu e-mail" type="email" />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
