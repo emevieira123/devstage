6a195f5db845db9001285f57f80f40140a065e20
Emerson Vieira <emevieira@gmail.com>
2025-02-22 19:15:24 -0400

    feat: install react-hook-form and add in forms

--- src/app/invite/_components/invite-link-input.tsx
+++ src/app/invite/_components/invite-link-input.tsx
@@ -1,8 +1,18 @@
+"use client"
+
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string;
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
@@ -11,10 +21,10 @@ export function InviteLinkInput() {
 
       <InputField
         readOnly
-        defaultValue="http://localhost:3000/invite/386486238487623846786"
+        defaultValue={inviteLink}
       />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -8,6 +8,8 @@ import { Ranking } from './_components/ranking'
 import { Stats } from './_components/stats'
 
 export default function InvitePage() {
+  const inviteLink = "http://localhost:3000/invite/386486238487623846786"
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className=" flex flex-col gap-10 w-full max-w-[550px]">
@@ -34,7 +36,7 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
 
           <Stats />
         </div>
